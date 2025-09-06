801a98370a9c1b6cdf66f305e19413ce805adb4a
Wes Chow <wes.chow@gmail.com>
2025-09-05 20:41:52 -0400

    Attempt to use proper cargo packaging.

--- cowsay-external/package/rust-cowsay/rust-cowsay.mk
+++ cowsay-external/package/rust-cowsay/rust-cowsay.mk
@@ -4,22 +4,10 @@
 #
 ################################################################################
 
-RUST_COWSAY_VERSION = master
-RUST_COWSAY_SITE = https://github.com/wesc/rust-cowsay
-RUST_COWSAY_SITE_METHOD = git
+RUST_COWSAY_VERSION = 4c85805169b87976d42b081966bafcc6a779e3a9
+RUST_COWSAY_SITE = $(call github,wesc,rust-cowsay,$(RUST_COWSAY_VERSION))
 RUST_COWSAY_LICENSE = MIT
 RUST_COWSAY_LICENSE_FILES = LICENSE
-
 RUST_COWSAY_DEPENDENCIES = host-rustc
 
-define RUST_COWSAY_BUILD_CMDS
-	cd $(@D) && \
-	$(HOST_DIR)/bin/cargo build --release --target-dir $(RUST_COWSAY_BUILDDIR)
-endef
-
-define RUST_COWSAY_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(RUST_COWSAY_BUILDDIR)/release/cowsay \
-		$(TARGET_DIR)/usr/bin/cowsay
-endef
-
-$(eval $(generic-package))
+$(eval $(cargo-package))
