9deae28babc205d113e670883678dc40fdd56e21
Wes Chow <wes.chow@gmail.com>
2025-09-06 00:41:28 -0400

    FIXUP this guy

--- cowsay-external/package/rust-cowsay/rust-cowsay.mk
+++ cowsay-external/package/rust-cowsay/rust-cowsay.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RUST_COWSAY_VERSION = 4c85805169b87976d42b081966bafcc6a779e3a9
+RUST_COWSAY_VERSION = 4b49e8118de14dd173185ae078c6b93fbceea749
 RUST_COWSAY_SITE = $(call github,wesc,rust-cowsay,$(RUST_COWSAY_VERSION))
 RUST_COWSAY_LICENSE = MIT
 RUST_COWSAY_LICENSE_FILES = LICENSE
