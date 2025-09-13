09f3eb3f043d4330871034248571030d26669d7c
Wes Chow <wes.chow@gmail.com>
2025-09-12 22:16:56 -0400

    Fix bindgen70

--- miles-external/package/rust-bindgen70/rust-bindgen70.mk
+++ miles-external/package/rust-bindgen70/rust-bindgen70.mk
@@ -6,10 +6,9 @@
 
 RUST_BINDGEN70_VERSION = 0.70.1
 RUST_BINDGEN70_SITE = $(call github,rust-lang,rust-bindgen,v$(RUST_BINDGEN70_VERSION))
-RUST_BINDGEN70_SOURCE = rust-bindgen-$(RUST_BINDGEN70_VERSION).tar.gz
+RUST_BINDGEN70_SOURCE = rust-bindgen-$(RUST_BINDGEN70_VERSION)-cargo2.tar.gz
 RUST_BINDGEN70_LICENSE = BSD-3-clause
 RUST_BINDGEN70_LICENSE_FILES = LICENSE
-RUST_BINDGEN70_DEPENDENCIES = llvm host-clang
 
 # The Cargo.toml at the root directory is a "virtual manifest".
 # Since we only want to build and install bindgen use the Cargo.toml
